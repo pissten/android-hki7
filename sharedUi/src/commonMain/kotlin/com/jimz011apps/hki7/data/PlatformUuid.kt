1b94ab6cdeb036c96be5e10057ee00d6ce9a252a
pissten <pissten@hotmail.com>
2026-08-05 00:15:00 +0200

    ci: validate Android fixes on migration branch

--- sharedUi/src/commonMain/kotlin/com/jimz011apps/hki7/data/PlatformUuid.kt
+++ sharedUi/src/commonMain/kotlin/com/jimz011apps/hki7/data/PlatformUuid.kt
@@ -1,4 +1,4 @@
 package com.jimz011apps.hki7.data
 
-/** Generates the opaque ids used by HKI 7 spacer and action items. */
+/** Generates the opaque ids used by shared HKI 7 spacer and action items. */
 internal expect fun hki7RandomUuid(): String
