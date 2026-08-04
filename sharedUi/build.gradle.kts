cb6c490b99a953b5e9741c57341e006872fa3586
pissten <pissten@hotmail.com>
2026-08-04 20:46:32 +0200

    feat(kmp): add shared Compose UI module

---
+++ sharedUi/build.gradle.kts
@@ -0,0 +1,30 @@
+import org.jetbrains.kotlin.gradle.ExperimentalWasmDsl
+
+plugins {
+    alias(libs.plugins.kotlin.multiplatform)
+    alias(libs.plugins.android.kotlin.multiplatform.library)
+    alias(libs.plugins.compose.multiplatform)
+    alias(libs.plugins.compose.compiler)
+}
+
+kotlin {
+    android {
+        namespace = "com.jimz011apps.hki7.sharedui"
+        compileSdk = 37
+        minSdk = 34
+    }
+
+    @OptIn(ExperimentalWasmDsl::class)
+    wasmJs {
+        browser()
+    }
+
+    sourceSets {
+        commonMain.dependencies {
+            implementation(compose.runtime)
+            implementation(compose.foundation)
+            implementation(compose.material3)
+            implementation(compose.ui)
+        }
+    }
+}
